a8df7072c796060150376fd756c5519062a6122f
Vicky Tsao <vickytsa@gmail.com>
2017-09-12 00:18:41 -0500

    Add files via upload

---
+++ PetCareOption.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace VickyTsao.PetCare.Objects
+{
+    public class PetCareOption
+    {
+        public int PetCareOptionId { get; set; }
+
+        public string PetCareOptionName { get; set; }
+
+    }
+}
---
+++ PetCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VickyTsao.PetCare.Objects
+{
+    public class PetCategory
+    {
+            
+        public int PetCategoryId { get; set; }
+
+        public string PetCategoryName { get; set; }
+    
+    }
+
+    
+}
