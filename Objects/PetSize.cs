d4716c931cd7fb220567b9f1f317ea972d3e8004
Vicky Tsao <vickytsa@gmail.com>
2017-09-12 00:46:30 -0500

    Add files via upload

---
+++ Objects/PetSize.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VickyTsao.PetCare.Objects
+{
+    public class PetSize
+    {
+        public string PetSizeId { get; set; }
+
+        public string PetSizeName { get; set; }
+    }
+}
