82ca8ca9710f6ebd6942e683499767e04afc0b3a
Vicky Tsao <vickytsa@gmail.com>
2017-09-12 00:56:27 -0500

    Add files via upload

---
+++ WebApi/Controllers/PetSizeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VickyTsao.PetCare.Objects;
+using VickyTsao.PetCare.Sql;
+
+namespace VickyTsao.PetCare.WebApi.Controllers
+{
+    public class PetSizeController : ApiController
+    {
+        private string _conn = ConfigurationManager.ConnectionStrings[ConfigurationManager.AppSettings["ConnectionString"]].ConnectionString;
+
+        // GET: api/PetSize
+        public IEnumerable<PetSize> Get()
+        {
+            var client = new SqlClient(_conn);
+
+            return client.GetAllPetSizeOptions();
+        }
+
+        // GET: api/PetSize/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/PetSize
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/PetSize/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/PetSize/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
